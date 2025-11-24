3da91b4085292e21f222ba61bff5fc3222941e47
maggy <perezmag5137@gmail.com>
2025-11-24 11:33:59 -0500

    moved all code into their own methods for organization

--- main.py
+++ main.py
@@ -5,39 +5,51 @@ import numpy as np
 import os
 from dotenv import load_dotenv
 
-# !pip install balldontlie
 from balldontlie import BalldontlieAPI
-# !pip install nba_api  # <-- Package required to install
 
 from nba_api.stats.endpoints import playercareerstats
 from nba_api.stats.static import teams, players
+
+
+def main():
+    # Calling all functions here
+    scrape_get_all_teams()
+    scrape_get_all_players_of_all_time()
+    scrape_single_player_by_id()
+    scrape_player_info_by_full_name()
+    scrape_ACTIVE_PLAYERS()
 
 
 """
 Title: Github NBA API webscrape
 """
 
-
 """
 1. Getting all teams and offloading them into a csv file
     "nba_teams.csv"
 """
-# getting all teams
-nba_teams = teams.get_teams()
+def scrape_get_all_teams():
+    
+    # getting all teams
+    nba_teams = teams.get_teams()
 
-df = pd.DataFrame(nba_teams)
-df.to_csv('nba_teams.csv')
-
+    df = pd.DataFrame(nba_teams)
+    df.to_csv('nba_teams.csv')
+    df
+    
+    
+    
 
 """
 2. Getting all players of all time
     "all_players.csv"
 """
-all_players = players.get_players()
+def scrape_get_all_players_of_all_time():
+    all_players = players.get_players()
 
-df = pd.DataFrame(all_players)
-df.to_csv("all_players.csv")
-df
+    df = pd.DataFrame(all_players)
+    df.to_csv("all_players.csv")
+    df
 
 
 
@@ -45,12 +57,14 @@ df
 3. Getting a single player by their id and offloading to a csv file
     "Nikola_Jokic_Info.csv"
 """
-career = playercareerstats.PlayerCareerStats(player_id='203999')
+def scrape_single_player_by_id():
+    career = playercareerstats.PlayerCareerStats(player_id='203999')
 
-info = career.get_data_frames()[0]
-df = pd.DataFrame(info)
-df.to_csv("Nikola_Jokic_Info.csv")
-df
+    info = career.get_data_frames()[0]
+    df = pd.DataFrame(info)
+    df.to_csv("Nikola_Jokic_Info.csv")
+    df
+
 
 
 
@@ -58,21 +72,22 @@ df
 4. Getting a player's info by their full name to see if the player is active
     "AlexAbrines.csv"
 """
-# var to get player's info and to see if the player is active
-name = "Alex Abrines"
+def scrape_player_info_by_full_name():
+    # var to get player's info and to see if the player is active
+    name = "Alex Abrines"
 
 
-# finding playyer by their full name
-player = players.find_players_by_full_name(name)
+    # finding playyer by their full name
+    player = players.find_players_by_full_name(name)
 
-df = pd.DataFrame(player)
+    df = pd.DataFrame(player)
 
 
-# removing whitespaces from player name to be used as a file name (e.g AlexAbrines.csv)
-csv_name = name.replace(" ", "")
+    # removing whitespaces from player name to be used as a file name (e.g AlexAbrines.csv)
+    csv_name = name.replace(" ", "")
 
-df.to_csv(f"{csv_name}.csv")
-df
+    df.to_csv(f"{csv_name}.csv")
+    df
 
 
 
@@ -86,36 +101,36 @@ Title: Ball don't lie API webscraping
 5. Getting all ACTIVE players and offloading to a csv file
     "ACTIVE_PLAYERS.csv"
 """
-import requests
-# from google.colab import userdata
+def scrape_ACTIVE_PLAYERS():
 
-load_dotenv()
-API_KEY = os.getenv('nba_api_key')
+    import requests
+    # from google.colab import userdata
 
-# All 30 NBA teams
-team_ids = list(range(1, 31))
+    load_dotenv()
+    API_KEY = os.getenv('nba_api_key')
 
-# Build team_id params
-team_params = "&".join([f"team_ids[]={t}" for t in team_ids])
+    # All 30 NBA teams
+    team_ids = list(range(1, 31))
 
-url = f"https://api.balldontlie.io/nba/v1/players?{team_params}&per_page=100"
+    # Build team_id params
+    team_params = "&".join([f"team_ids[]={t}" for t in team_ids])
 
-headers = {
-    "Authorization": API_KEY
-}
+    url = f"https://api.balldontlie.io/nba/v1/players?{team_params}&per_page=100"
 
-# making a request to the api, and then getting a response
-response = requests.get(url, headers=headers)
-data = response.json()
+    headers = {
+        "Authorization": API_KEY
+    }
 
-df = pd.json_normalize(data["data"])
+    # making a request to the api, and then getting a response
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
-# offloading to csv file
-df.to_csv("ACTIVE_PLAYERS.csv", index=False)
+    df = pd.json_normalize(data["data"])
 
-print("Active players scraped:", len(df))
+    # offloading to csv file
+    df.to_csv("ACTIVE_PLAYERS.csv", index=False)
 
-
-
-
+    print("Active players scraped:", len(df))
     
+
+main()
